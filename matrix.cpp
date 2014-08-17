e30c9653fa3b8412c42558410b5b9d02377c66b4
Jeremy Linton <jlinton@usegithub>
2014-08-17 01:06:27 -0500

    Update copyright

--- matrix.cpp
+++ matrix.cpp
@@ -1,8 +1,33 @@
+// Copyright (c) 2012,2014 Jeremy Linton
+//
+// matrix.cpp
+//   C++ wrapper which creates a 4x4 matrix out of the x4 vector classes
+//   This matrix class works with the NEON,SSE and PORT classes
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+
+
 // This class wraps one of the vector types to create a basic 4x4 matrix class
 // Its pretty "clean" except for the Transpose operation which requires us to be 
 // friended by the Vec4 class so that we can access the individual vector members in a 
 // somewhat efficient manner. 
-
 
 
 
